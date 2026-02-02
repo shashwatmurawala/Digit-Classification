0683bfb04e50004aaeabfe6987806da5bf7b8680
Shashwat Murawala <murawala.shashwat@gmail.com>
2026-02-02 16:08:32 +0900

    Added training for the model

--- main.py
+++ main.py
@@ -28,20 +28,81 @@ transform = transforms.Compose([
 train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
-image, label = train_dataset[0]
+# Define the fully connected model
+class FullyConnectedModel(nn.Module):
+    def __init__(self):
+        super(FullyConnectedModel, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(28 * 28, 128)
 
-print(image.size())
-print(label)
+        # '''TODO: Define the activation function for the first fully connected layer'''
+        self.relu = nn.ReLU()
 
-plt.figure(figsize=(10,10))
-random_inds = np.random.choice(60000,36)
-for i in range(36):
-    plt.subplot(6, 6, i + 1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    image_ind = random_inds[i]
-    image, label = train_dataset[image_ind]
-    plt.imshow(image.squeeze(), cmap=plt.cm.binary)
-    plt.xlabel(label)
-comet_model_1.log_figure(figure=plt.gcf)
+        # '''TODO: Define the second Linear layer to output the classification probabilities'''
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.fc1(x)
+
+        # '''TODO: Implement the rest of forward pass of the model using the layers you have defined above'''
+        x = self.relu(x)
+        x = self.fc2(x)
+
+        return x
+        
+
+fc_model = FullyConnectedModel().to(device) # send the model to GPU
+
+'''TODO: Experiment with different optimizers and learning rates. How do these affect
+    the accuracy of the trained model? Which optimizers and/or learning rates yield
+    the best performance?'''
+# Define loss function and optimizer
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(fc_model.parameters(), lr=0.001)
+
+# Create DataLoaders for batch processing
+BATCH_SIZE = 64
+trainset_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+testset_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+def train(model, dataloader, criterion, optimizer, epochs):
+    model.train()  # Set the model to training mode
+    for epoch in range(epochs):
+        total_loss = 0
+        correct_pred = 0
+        total_pred = 0
+
+        for images, labels in trainset_loader:
+            # Move tensors to GPU so compatible with model
+            images, labels = images.to(device), labels.to(device)
+
+            # Forward pass
+            outputs = fc_model(images)
+
+            # Clear gradients before performing backward pass
+            optimizer.zero_grad()
+            # Calculate loss based on model predictions
+            loss = loss_function(outputs, labels)
+            # Backpropagate and update model parameters
+            loss.backward()
+            optimizer.step()
+
+            # multiply loss by total nos. of samples in batch
+            total_loss += loss.item()*images.size(0)
+
+            # Calculate accuracy
+            predicted = torch.argmax(outputs, dim=1)  # Get predicted class
+            correct_pred += (predicted == labels).sum().item()  # Count correct predictions
+            total_pred += labels.size(0) # Count total predictions
+
+        # Compute metrics
+        total_epoch_loss = total_loss / total_pred
+        epoch_accuracy = correct_pred / total_pred
+        print(f"Epoch {epoch + 1}, Loss: {total_epoch_loss}, Accuracy: {epoch_accuracy:.4f}")
+
+# TODO: Train the model by calling the function appropriately
+EPOCHS = 5
+train(fc_model, trainset_loader, loss_function, optimizer, EPOCHS) # TODO
+
+comet_model_1.end()
